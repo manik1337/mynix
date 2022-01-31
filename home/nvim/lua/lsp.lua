defdbbf8378e3038fcc0e8849c0743021c983cdb
Dmitry Manik <manikdmitry@gmail.com>
2022-01-31 11:03:01 +0300

    add nvim lsp shortcuts

--- home/nvim/lua/lsp.lua
+++ home/nvim/lua/lsp.lua
@@ -32,25 +32,61 @@ cmp.setup({
   }),
 })
 
+local opts = { noremap=true, silent=true }
+vim.api.nvim_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+vim.api.nvim_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+vim.api.nvim_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+vim.api.nvim_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
+
+local on_attach = function(client, bufnr)
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+end
+
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
-local lsp = require'lspconfig'
-
--- Erlang
-require'lspconfig'.erlangls.setup{
-  capabilities = capabilities
-}
-
--- Nix
-require'lspconfig'.rnix.setup{}
-
--- Rust
-require'lspconfig'.rust_analyzer.setup{
-  settings = {
-    rust = {
-      unstable_features = true,
-      build_on_save = false,
-      all_features = true
-    }
+local servers = { 'erlangls', 'rnix', 'rust_analyzer' }
+for _, server in pairs(servers) do
+  require('lspconfig')[server].setup {
+    on_attach = on_attach,
+    flags = { debounce_text_changes = 150, },
+    capabilities = capabilities
   }
-}
+end
+
+--require('rust-tools').setup({})
+
+-- -- Erlang
+-- lsp.erlangls.setup{
+--   on_attach = on_attach,
+--   capabilities = capabilities
+-- }
+-- 
+-- -- Nix
+-- lsp.rnix.setup{
+--   on_attach = on_attach
+-- }
+-- 
+-- -- Rust
+-- require('rust-tools').setup({})
+-- 
+-- lsp.rust_analyzer.setup{
+--   on_attach = on_attach,
+--   settings = {
+--     rust = {
+--       unstable_features = true,
+--       build_on_save = false,
+--       all_features = true
+--     }
+--   }
+-- }
