4d23a727d8a0f9557e15b759d4d2d9a9bef0a2cb
Dmitry Manik <manikdmitry@gmail.com>
2022-06-09 20:02:16 +0300

    update discord

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,10 +1,10 @@
 self: super:
 rec {
   discord = super.discord.overrideAttrs (old: rec {
-    version = "0.0.17";
+    version = "0.0.18";
     src = super.fetchurl {
       url = "https://dl.discordapp.net/apps/linux/${version}/discord-${version}.tar.gz";
-      sha256 = "058k0cmbm4y572jqw83bayb2zzl2fw2aaz0zj1gvg6sxblp76qil";
+      sha256 = "1hl01rf3l6kblx5v7rwnwms30iz8zw6dwlkjsx2f1iipljgkh5q4";
     };
   });
 
