20e2386c0a252ec15f5f5e11132c28e8aee69353
Dmitry Manik <manikdmitry@gmail.com>
2022-07-13 22:01:31 +0300

    Update waybar config

--- home/waybar.nix
+++ home/waybar.nix
@@ -8,16 +8,24 @@
         modules-left = [ "sway/workspaces" "sway/mode" "wlr/taksbar" ];
         modules-center = [ "sway/window" ];
         modules-right = [
-          "temperature"
           "sway/language"
           "tray"
+          "temperature#cpu"
           "cpu"
           "memory"
           "battery"
-          "clock"
+          "clock#time"
+          "clock#date"
         ];
 
         modules = {
+          "temperature#cpu" = {
+            interval = 1;
+            hwmon-path = "/sys/class/hwmon/hwmon2/temp3_input";
+            format = " {temperatureC}°C";
+            tooltip = false;
+          };
+
           "cpu" = { format = "{usage}% "; };
 
           "memory" = { format = "{}% "; };
@@ -35,6 +43,18 @@
           "tray" = {
             icon-size = 20;
             spacing = 8;
+          };
+          "clock#time" = {
+            interval = 1;
+            format = "{:%H:%M:%S}";
+            tooltip = false;
+          };
+
+          "clock#date" = {
+            format = " {:%e %b %Y}";
+            tooltip-format = ''
+              <big>{:%Y %B}</big>
+              <tt><small>{calendar}</small></tt>'';
           };
         };
       };
