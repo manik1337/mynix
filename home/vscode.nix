bcf7c84e5110a6952a5f6a1c0da8eeea1b43a634
Dmitry Manik <manikdmitry@gmail.com>
2022-08-04 12:25:42 +0300

    Update vs code font and theme

--- home/vscode.nix
+++ home/vscode.nix
@@ -8,13 +8,11 @@
       justusadam.language-haskell
       haskell.haskell
       jakebecker.elixir-ls
-
     ];
     userSettings = {
-      editor.fontFamily = "Hack Nerd Font";
+      editor.fontFamily = "IBM Plex Mono";
       editor.fontSize = 14;
       telemetry.enableTelemetry = false;
-      workbench.colorTheme = "Tokyo Night";
       workbench.iconTheme = "vscode-icons";
     };
   };
