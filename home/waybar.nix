ce313027845ec40a3fd069d6c79a1095104c0055
Dmitry Manik <manikdmitry@gmail.com>
2022-10-03 12:36:59 +0300

    Update waybar config

--- home/waybar.nix
+++ home/waybar.nix
@@ -4,64 +4,71 @@
     systemd.enable = true;
     settings = {
       mainBar = {
-        position = "top";
-        modules-left = [ "sway/workspaces" "sway/mode" "wlr/taksbar" ];
+        position = "bottom";
+        # modules-left = [ "sway/workspaces" "sway/mode" "wlr/taksbar" ];
+        modules-left = [ "sway/workspaces" "sway/mode" ];
         modules-center = [ ];
         modules-right = [
-          "sway/language"
           "tray"
-          "temperature#cpu"
+          "custom/spotify"
+          "custom/disk_root"
           "cpu"
           "memory"
           "battery"
           "clock#time"
           "clock#date"
+          "sway/language"
         ];
 
         modules = {
-          "temperature#cpu" = {
-            interval = 1;
-            hwmon-path = "/sys/class/hwmon/hwmon2/temp3_input";
-            format = " {temperatureC}°C";
-            tooltip = false;
+          "sway/workspaces" = {
+            disable-scroll = true;
+            all-outputs = true;
           };
 
-          "cpu" = {
-            format = ''
-               {usage}%'';
+          "custom/spotify" = {
+            exec =
+              "/usr/bin/python3 /full/path/to/mediaplayer.py --player spotify";
+            max-length = 40;
+            format = " {}";
+            return-type = "json";
+            on-click = "playerctl -p spotify play-pause";
           };
 
-          "memory" = {
-            format = ''
-               {}%'';
+          "custom/disk_root" = {
+            format = "DISK {}";
+            interval = 30;
+            exec = "df -h --output=avail / | tail -1 | tr -d ' '";
           };
+
+          "cpu" = { format = "CPU {usage}%"; };
+
+          "memory" = { format = "RAM {}%"; };
 
           "battery" = {
             states = {
               warning = 20;
               critical = 10;
             };
-            format = "{capacity}%";
-            format-discharging = "{capacity}%";
+            format = "POWER {capacity}%";
+            format-discharging = "BAT {capacity}%";
             tooltip = true;
           };
 
           "tray" = {
             icon-size = 20;
-            spacing = 2;
+            spacing = 4;
           };
 
           "clock#time" = {
             interval = 1;
-            format = ''
-              {:%H:%M}'';
+            format = "{:%H:%M}";
             tooltip = false;
           };
 
           "clock#date" = {
             # format = " {:%e\n%b}";
-            format = ''
-              {:%e %b}'';
+            format = "{:%e %b}";
             tooltip-format = ''
               <big>{:%Y %B}</big>
               <tt><small>{calendar}</small></tt>'';
@@ -69,5 +76,69 @@
         };
       };
     };
+    style = ''
+      * {
+        border: none;
+        border-radius: 0;
+        font-family: Hack;
+        font-size: 13px;
+        min-height: 0;
+      }
+      window#waybar {
+        background: transparent;
+        color: white;
+      }
+      #window {
+        font-weight: bold;
+      }
+      #workspaces button {
+        padding: 0 5px;
+        background: transparent;
+        color: white;
+        border-top: 2px solid transparent;
+      }
+      #workspaces button.focused {
+        color: #c9545d;
+        border-top: 2px solid #c9545d;
+      }
+      #mode {
+        background: #64727D;
+        border-bottom: 3px solid white;
+      }
+      #clock, #battery, #cpu, #memory, #network, #pulseaudio, #custom-spotify, #tray, #mode {
+        padding: 0 3px;
+        margin: 0 2px;
+      }
+      #clock {
+        font-weight: bold;
+      }
+      #battery {
+      }
+      #battery icon {
+        color: red;
+      }
+      #battery.charging {
+      }
+      @keyframes blink {
+        to {
+          background-color: #ffffff;
+          color: black;
+        }
+      }
+      #battery.warning:not(.charging) {
+        color: white;
+        animation-name: blink;
+        animation-duration: 0.5s;
+        animation-timing-function: linear;
+        animation-iteration-count: infinite;
+        animation-direction: alternate;
+      }
+      #cpu {
+      }
+      #memory {
+      }
+      #tray {
+      }
+    '';
   };
 }
