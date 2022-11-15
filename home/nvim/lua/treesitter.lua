d3e2d85a396976dd2b7dd856833e16e66a39caae
Dmitrii Manik <manikdmitry@gmail.com>
2022-11-15 19:20:42 +0200

    update

--- home/nvim/lua/treesitter.lua
+++ home/nvim/lua/treesitter.lua
@@ -25,26 +25,5 @@ require'nvim-treesitter.configs'.setup {
     enable = true,
     use_languagetree = false,
     additional_vim_regex_highlighting = false,
-  },
-
-  refactor = {
-    highlight_definitions = { enable = true },
-    highlight_current_scope = { enable = false },
-
-    smart_rename = {
-      enable = true,
-      keymaps = {
-        -- mapping to rename reference under cursor
-        smart_rename = "grr",
-      },
-    },
-
-    navigation = {
-      enable = false,
-      keymaps = {
-        goto_definition = "gnd", -- mapping to go to definition of symbol under cursor
-        list_definitions = "gnD", -- mapping to list all definitions in current file
-      },
-    },
-  },
+  }
 }
