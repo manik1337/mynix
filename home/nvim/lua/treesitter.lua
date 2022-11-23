ed72d75c585d04f9e373ea2f159558114f77d20f
Dmitrii Manik <manikdmitry@gmail.com>
2022-11-23 18:05:37 +0200

    Remove grammas from treesitter config

--- home/nvim/lua/treesitter.lua
+++ home/nvim/lua/treesitter.lua
@@ -1,24 +1,5 @@
 require'nvim-treesitter.configs'.setup {
   parser_install_dir = "~/.local/share/nvim/site",
-  ensure_installed = {
-    "elixir",
-    "erlang",
-    "rust",
-    "go",
-    "javascript",
-    "python",
-    "typescript",
-    "solidity",
-    "ocaml",
-    "haskell",
-    "scala",
-
-    "nix",
-    "lua",
-    "dockerfile",
-    "json",
-    "markdown"
-  },
   sync_install = false,
 
   highlight = {
