2774317abedf17fa5ac2542fab1ef54a45e9550b
Dmitrii Manik <manikdmitry@gmail.com>
2023-01-10 12:08:35 +0200

    Remove tab lsp completion so it doesn't conflict with copilot

--- home/nvim/lua/lsp.lua
+++ home/nvim/lua/lsp.lua
@@ -13,10 +13,10 @@ cmp.setup({
     ['<C-p>'] = cmp.mapping.select_prev_item(),
     ['<C-n>'] = cmp.mapping.select_next_item(),
     -- Add tab support
-    ['<S-Tab>'] = cmp.mapping.select_prev_item(),
-    ['<Tab>'] = cmp.mapping.select_next_item(),
-    --['<C-d>'] = cmp.mapping.scroll_docs(-4),
-    --['<C-f>'] = cmp.mapping.scroll_docs(4),
+    -- ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+    -- ['<Tab>'] = cmp.mapping.select_next_item(),
+    -- ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    -- ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.close(),
     ['<CR>'] = cmp.mapping.confirm({
