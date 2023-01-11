3d830e47fbe7f6a2ca9c1ad7509612624170dc13
Dmitrii Manik <manikdmitry@gmail.com>
2023-01-11 07:41:26 +0200

    format

--- home/nvim/lua/nvim-tree.lua
+++ home/nvim/lua/nvim-tree.lua
@@ -1,36 +1,36 @@
 require'nvim-tree'.setup {
-    disable_netrw       = true,
-    hijack_netrw        = true,
-    open_on_setup       = false,
-    ignore_ft_on_setup  = {},
+    disable_netrw      = true,
+    hijack_netrw       = true,
+    open_on_setup      = false,
+    ignore_ft_on_setup = {},
 
-    open_on_tab         = false,
-    hijack_cursor       = false,
-    update_cwd          = false,
-    diagnostics         = {
-        enable = true,
-        icons = {
-            hint = "",
-            info = "",
-            warning = "",
-            error = "",
-        }
+    open_on_tab        = false,
+    hijack_cursor      = false,
+    update_cwd         = false,
+    diagnostics        = {
+      enable = true,
+      icons = {
+        hint    = "",
+        info    = "",
+        warning = "",
+        error   = "",
+      }
     },
     update_focused_file = {
-        enable      = false,
-        update_cwd  = false,
-        ignore_list = {}
+      enable      = false,
+      update_cwd  = false,
+      ignore_list = {}
     },
     system_open = {
-        cmd  = nil,
-        args = {}
+      cmd  = nil,
+      args = {}
     },
     view = {
-        side = 'left',
-        mappings = {
-            custom_only = false,
-            list = {}
-        }
+      side = 'left',
+      mappings = {
+        custom_only = false,
+        list = {}
+      }
     }
 }
 
