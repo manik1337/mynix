7f005bd2c22708d98dd756d220d7f4880299a045
Dmitrii Manik <manikdmitry@gmail.com>
2023-01-17 14:22:00 +0200

    add zellij theme

--- home/zellij.nix
+++ home/zellij.nix
@@ -2,5 +2,23 @@
 {
   programs.zellij = {
     enable = true;
+    settings = {
+      theme = "base16";
+      themes = {
+        base16 = {
+          fg      = "#c5c8c6";
+          bg      = "#1d1f21";
+          black   = "#1d1f21";
+          red     = "#cc6666";
+          green   = "#b5bd68";
+          yellow  = "#f0c674";
+          blue    = "#81a2be";
+          magenta = "#b294bb";
+          cyan    = "#8abeb7";
+          white   = "#c5c8c6";
+          orange  = "#dc9656";
+        };
+      };
+    };
   };
 }
