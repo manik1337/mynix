72797475528d53e81e42db1cb18947ff05c86502
Dmitrii Manik <manikdmitry@gmail.com>
2023-04-21 08:12:13 +0300

    update waybar

--- home/waybar/default.nix
+++ home/waybar/default.nix
@@ -5,50 +5,120 @@
     systemd.enable = true;
     settings = {
       mainBar = {
-        position = "left";
+        position = "top";
         modules-left = [ "sway/workspaces"];
-        modules-center = [ ];
+        modules-center = [ "clock" ];
         modules-right = [
-          "cpu"
-          "memory"
-          "clock"
-          "battery"
-          "sway/language"
           "tray"
+          "custom/separator"
+          "cpu"
+          "custom/separator"
+          "memory"
+          "custom/separator"
+          "backlight"
+          "custom/separator"
+          "pulseaudio"
+          "custom/separator"
+          "network"
+          "custom/separator"
+          "battery"
+          "custom/separator"
+          "sway/language"
         ];
 
         modules = {
           "sway/workspaces" = {
             disable-scroll = true;
-            all-outputs = true;
           };
 
-          "cpu" = { format = " {usage}%"; };
+          "cpu" = {
+            format = " {usage}%";
+          };
 
-          "memory" = { format = " {}%"; };
+          "memory" = {
+            format = " {}%";
+          };
 
           "battery" = {
             interval = 60;
             states = {
-              warning = 20;
-              critical = 10;
+              good = 95;
+              warning = 30;
+              critical = 15;
             };
-            format = "{capacity}{icon}";
-            format-charging = "{capacity}% ";
-            format-plugged = "{capacity}% ";
-            format-alt = "{time} {icon}";
+            format = "{icon}  {capacity}%";
+            format-charging = " {capacity}%";
+            format-plugged = " {capacity}%";
+            format-alt = "{icon}  {time}";
             format-icons = ["" "" "" "" ""];
             tooltip = true;
           };
 
           "tray" = {
-            spacing = 0;
+            spacing = 10;
           };
 
           "clock" = {
-              format = "{:%R}";
-              tooltip-format = "<big>{:%Y %B}</big>\n<tt><small>{calendar}</small></tt>";
-              format-alt = "{:%Y-%m-%d}";
+            format = "{:%H:%M %A, %B %d, %Y}  ";
+            tooltip-format = "<tt><small>{calendar}</small></tt>";
+            # calendar = {
+            #   mode = "year";
+            #   mode-mon-col = 3;
+            #   weeks-pos = "right";
+            #   on-scroll = 1;
+            #   on-click-right = "mode";
+            #   format = {
+            #     months = "<span color='#ffead3'><b>{}</b></span>";
+            #     days = "<span color='#ecc6d9'><b>{}</b></span>";
+            #     weeks = "<span color='#99ffdd'><b>W{}</b></span>";
+            #     weekdays = "<span color='#ffcc66'><b>{}</b></span>";
+            #     today = "<span color='#ff6699'><b><u>{}</u></b></span>";
+            #   };
+            # };
+            # actions = {
+            #   on-click-right = "mode";
+            #   on-click-forward = "tz_up";
+            #   on-click-backward = "tz_down";
+            #   on-scroll-up = "shift_up";
+            #   on-scroll-down = "shift_down";
+            # };
+          };
+
+          "network" = {
+            format-wifi = "  {essid}";
+		        format-ethernet = "  Connected";
+		        format-disconnected = " Disconnected";
+		        tooltip-format-wifi = " {ifname} @ {essid}\nIP: {ipaddr}\nStrength: {signalStrength}%\nFreq: {frequency}MHz\n⬆️ {bandwidthUpBits} ⬇️ {bandwidthDownBits}";
+		        tooltip-format-ethernet = " {ifname}\nIP: {ipaddr}\n⬆️ {bandwidthUpBits} ⬇️ {bandwidthDownBits}";
+		        format-linked = "{ifname} (No IP) ";
+          };
+
+          "pulseaudio" = {
+            format = "{volume}% {icon} {format_source}";
+            format-bluetooth = "{volume}%  {format_source}";
+            format-bluetooth-muted = "   {format_source}";
+            format-muted = " {format_source}";
+            format-source = "{volume}%  ";
+            format-source-muted = "  ";
+            format-icons = {
+              headphone = "";
+              hands-free = "";
+              headset = "";
+              phone = "";
+              portable = "";
+              car = "";
+              default = ["" "" ""];
+            };
+            # on-click = "pavucontrol";
+          };
+
+          "custom/separator" = {
+            format = " | ";
+          };
+
+          "backlight" = {
+            format = "{icon} {percent}%";
+            format-icons = ["" "" "" "" "" "" "" ""];
           };
         };
       };
