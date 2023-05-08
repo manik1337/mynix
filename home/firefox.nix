e00e0889766b245c56036d7843261fe85c74c5a9
Dmitrii Manik <manikdmitry@gmail.com>
2023-05-08 15:56:32 +0300

    use unstable ff

--- home/firefox.nix
+++ home/firefox.nix
@@ -2,7 +2,7 @@
   programs = {
     firefox = {
       enable = true;
-      package = pkgs.stable.firefox-wayland;
+      package = pkgs.firefox-wayland;
       profiles.dmanik = {
         id = 0;
         name = "dmanik";
