ffefabf753b02ff1a04c3c7d0eb21d85c7b903b1
Dmitrii Manik <manikdmitry@gmail.com>
2024-12-29 13:23:20 +0100

    remove unused overlays

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,10 +1,2 @@
-{ inputs, ... }:
-let
-  nvim-plugins = import ./nvim-plugins.nix { inherit inputs; };
-  # ledger-live = import ./ledger-live.nix { inherit inputs; };
-  # ledger-live = inputs.callPackage ./ledger-live.nix { };
-in
-[
-  nvim-plugins
-  # ledger-live
-]
+inputs: system: final: prev: {
+}
